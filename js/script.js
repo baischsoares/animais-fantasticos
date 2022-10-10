270306696c6e0aae93303763a5be5e8508777e67
baischsoares <baischsoares@gmail.com>
2022-10-10 08:52:56 -0300

    accordion refatorado com classe

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-import initAccordion from './modules/accordion.js';
+import ScrollSuave from './modules/scroll-suave.js';
+import Accordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
@@ -10,10 +11,14 @@ import initFetchBitcoin from './modules/fetch-bitcoin.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 
 
+
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]')
 scrollSuave.init()
 
-initAccordion();
+const accordion = new Accordion('[data-anime="accordion"] dt')
+accordion.init()
+
+
 initTabNav();
 initModal();
 initTooltip();
